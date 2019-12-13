419f520d72988412ca1d58815fd1d66a643bfcf0
Wojciech Kojat <wkojat@gmail.com>
2019-12-13 21:31:59 +0100

    design compressive strengths for concrete classes, map container used, minor comment fix

--- concrete_classes_compressive_strengths.cpp
+++ concrete_classes_compressive_strengths.cpp
@@ -1,9 +1,9 @@
 /*english
-program determines design compressive strength of concrete class chosen by user;
+program determines design compressive strength of a concrete class chosen by user;
 a 'map' container was used to hold concrete classes and their corresponding compressive strentghs - fcd;
 program supports situation when incorrect concrete class symbol was entered by user;
 functions specific for containers from STL library were used;
-(consrete's design compressive strength is one of parameters used in calculations for civil engineering projects);
+(concrete's design compressive strength is one of parameters used in calculations for civil engineering projects);
 */
 
 /*polski
@@ -19,7 +19,7 @@ użyto funkcji właściwych kontenerom z biblioteki STL;
 #include <string>       //string object
 using namespace std;
 
-int main ()
+int main()
 {
     map <string, double> concrete_classes =
         {
@@ -36,9 +36,9 @@ int main ()
     cout <<"Enter concrete class: C12/15, C16/20, C20/25, C25/30 or C30/37: ";
     cin >>chosen_concrete_class;
 
-    map <string, double>::iterator result = concrete_classes.find (chosen_concrete_class);       //explanations below
+    map <string, double>::iterator result = concrete_classes.find(chosen_concrete_class);       //explanations below
 
-    if (result == concrete_classes.end ())                   //if no key was found
+    if (result == concrete_classes.end())                   //if no key was found
         cout <<"Incorrect concrete class" <<endl;
     else
     {
@@ -47,17 +47,17 @@ int main ()
              <<" concrete class is fcd = " <<determined_concrete_strength <<" MPa." <<endl;
     }
 
-    system ("pause");
+    system("pause");
 }
 
 /*
 additional explanations:
 1.  concrete classes and their fcd values are stored in a map container
-2.  user chosen concrete class is assigned to string object
-3.  STL specific function 'find' searches for that key in map and returns a iterator to specific element
+2.  user chosen concrete class is assigned to a string object
+3.  STL specific function 'find' searches for that key in the map and returns an iterator to the specific element
 4.  if no key was found, function returns iterator to map::end, which referrs to a 'theoretical' element
     that would follow the last one (same as function 'end')
-5.  if a key was found, corresponding mapped value is assigned determined_concrete_strength variable
+5.  if the key was found, corresponding mapped value is assigned to determined_concrete_strength variable
 6.  result->second refers to the second element of the pair (corresponding value in the map)
     (dereferencing map's iterator gives a std::pair<const key_tye, mapped_type>, which key and its corresponding value
     can be accessed through two members: first and second)
