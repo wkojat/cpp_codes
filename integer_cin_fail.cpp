d9725e9b93e76e640e4a427ad556f110e89452af
Wojciech Kojat <wkojat@gmail.com>
2020-02-27 20:22:40 +0100

    additional situation solved, when user enters e.g. 14gh-b

--- integer_cin_fail.cpp
+++ integer_cin_fail.cpp
@@ -1,3 +1,10 @@
+/*
+zabezpieczenie przed wpisaniem przez użytkownika nieprawidłowej wartości dla zmiennej typu int
+program obsługuje takie niepoprawne warianty jak np.: "jsd.b", "rw78cx-" albo "14gnk"
+(czyli gdy w strumieniu nie ma liczby, liczba znajduje się pomiędzy innymi znakami
+albo przed innymi znakami)
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -19,12 +26,18 @@ int main ()
         }
         else
         {
-            cout <<"number entered correctly" <<endl;
             getline(cin, garbage);
-            break;
+            if(garbage.empty())
+            {
+                cout <<"number entered correctly" <<endl;
+                break;
+            }
+            else
+            {
+                cout <<"error: typed characters are not an integer number" <<endl;
+            }
         }
     }
-
     cout <<"entered number is " <<a <<endl;
 
     system ("pause");
