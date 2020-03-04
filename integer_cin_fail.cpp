98d8a50c0e2439307c0af925bdf847889f0f42b1
Wojciech Kojat <wkojat@gmail.com>
2020-03-04 20:21:35 +0100

    changed for string object constructor and destructor to be called only once, not with each loop run

--- integer_cin_fail.cpp
+++ integer_cin_fail.cpp
@@ -2,7 +2,8 @@
 zabezpieczenie przed wpisaniem przez użytkownika nieprawidłowej wartości dla zmiennej typu int
 program obsługuje takie niepoprawne warianty jak np.: "jsd.b", "rw78cx-" albo "14gnk"
 (czyli gdy w strumieniu nie ma liczby, liczba znajduje się pomiędzy innymi znakami
-albo przed innymi znakami)
+albo przed innymi znakami); zaakceptowana zostanie jedynie sytuacja, gdy wpisana zostanie sama
+liczba całkowita (plus ewentualne białe znaki - sytuacja domyślna)
 */
 
 #include <iostream>
@@ -11,34 +12,38 @@ using namespace std;
 
 int main ()
 {
-    int a {0};
+    int value {0};
+    string garbage;
 
     while (true)
     {
-        string garbage;
+        int temp;
         cout <<"enter integer number: ";
-        cin >>a;
+        cin >>temp;
         if (cin.fail())
         {
             cout <<"error: typed characters are not an integer number" <<endl;
             cin.clear(cin.rdstate() & ~ios::failbit);
             getline(cin, garbage);
+            garbage.clear();
         }
         else
         {
             getline(cin, garbage);
             if(garbage.empty())
             {
+                value = temp;
                 cout <<"number entered correctly" <<endl;
                 break;
             }
             else
             {
                 cout <<"error: typed characters are not an integer number" <<endl;
+                garbage.clear();
             }
         }
     }
-    cout <<"entered number is " <<a <<endl;
+    cout <<"entered number is " <<value <<endl;
 
     system ("pause");
 }
